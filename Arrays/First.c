9a3a9696fe0f9970e8902d1594bb9b5b62465681
Shrey K <srkkadia@gmail.com>
2024-01-04 22:18:29 +0530

    Arrays

--- Arrays/First.c
+++ Arrays/First.c
@@ -1,7 +1,43 @@
 #include<stdio.h>
+// #define SIZE 10
+
+void rushingStuff(char topics[][10], int r, int c) {
+    printf("topics[%d][%d] = { ", r, c);
+    for(int i=0; i<r; i++) {
+        printf("\"");
+        for (int j = 0; j < c; j++) {
+            printf("%c", topics[i][j]);
+        }
+        printf("\", ");
+    }
+    printf("\b\b }");
+}
 
 void main() {
-    //
+    // int arr[10] = {0};    //declared
+    // int size = sizeof(arr)/sizeof(arr[0]);
+
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // arr[2] = 435;
+    // arr[6] = 234;
+
+    // for(int i=0; i<size; i++) {
+    //     printf("\nEnter arr[%d] = ", i);
+    //     scanf("%d", &arr[i]);
+    // }
+
+    // printf("\narr[%d] = { ", size);
+    // for(int i=0; i<size; i++) {
+    //     printf("%d, ", arr[i]);
+    // }
+    // printf("\b\b }");
+
+    // printf("%u", arr);
+
+    char arr[][10] = {"C", "C++", "Java", "Python"};
+    rushingStuff(arr, 4, 10);
+
 }
 /*
 declaration: allocating the memory to the array
