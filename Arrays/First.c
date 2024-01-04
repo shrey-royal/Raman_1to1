4b984a7d7f08f1b64fdbb1f832a3a9cca2094d5a
Shrey K <srkkadia@gmail.com>
2024-01-04 18:14:00 +0530

    function, array(intro)

---
+++ Arrays/First.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main() {
+    //
+}
+/*
+declaration: allocating the memory to the array
+defination: assigning the values to the array
+accessing the elements: getting specific elements/values by giving the index numbers
+
+//declaration
+datatype arrayname[size];
+
+//definition
+datatype arrname[] = {2, 1, 2, 4, 1, 23, 4};
+
+arr[0] = 2;
+arr[1] = 3;
+arr[1] = 345;   //overwrite the previous one
+
+*/
